b46c106fd5f60a717b711faad62c50989ddf7f0a
hxl <1150189655@qq.com>
2018-10-24 07:56:03 +0800

    sort out frequently appeared words

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import data
-import pase
+import parse
 if __name__ == "__main__":
     article = data.input_article('article/1.txt')
     sentences = parse.getsentences(article)
     dic = parse.makedic(article)
+    index = {}
+    mwords = sorted(dic.items(), key=lambda x:x[1], reverse=1)[0:3000]
+    for idx,s in enumerate(mwords):
+        index[s[0]] = idx
