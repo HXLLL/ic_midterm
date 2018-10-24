7f7b092c80ea63b26bec2e5de6ea503a1fb5a5db
hxl <1150189655@qq.com>
2018-10-24 13:23:25 +0800

    complete test of seperate position

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ global TOTAL
 TOTAL = 1000
 
 def input_parse():
-    datafile = 'data2.dat'
+    datafile = 'data3.dat'
     article = input_article('article/quora_questions_gbk.txt')
     sentences = parse.getsentences(article)
     print("sentence parsed")
@@ -21,7 +21,7 @@ def input_parse():
         index[s[0]] = idx
 
     print("calcaluting mat")
-    mat = np.zeros((TOTAL,TOTAL))
+    mat = np.zeros((TOTAL,4*TOTAL))
     S = 0
     #pdb.set_trace()
     for s in sentences:
@@ -31,18 +31,18 @@ def input_parse():
         for idx,w in enumerate(words):
             if not w in index: continue
             wid = index[w]
-            if idx-3 >= 0 and words[idx-3] in index: mat[wid][index[words[idx-3]]] += 0.5
+            #if idx-3 >= 0 and words[idx-3] in index: mat[wid][index[words[idx-3]]] += 0.5
             if idx-2 >= 0 and words[idx-2] in index: mat[wid][index[words[idx-2]]] += 1
-            if idx-1 >= 0 and words[idx-1] in index: mat[wid][index[words[idx-1]]] += 2
-            if idx+1 < len(words) and words[idx+1] in index: mat[wid][index[words[idx+1]]] += 2
-            if idx+2 < len(words) and words[idx+2] in index: mat[wid][index[words[idx+2]]] += 1
-            if idx+3 < len(words) and words[idx+3] in index: mat[wid][index[words[idx+3]]] += 0.5
+            if idx-1 >= 0 and words[idx-1] in index: mat[wid][TOTAL+index[words[idx-1]]] += 1
+            if idx+1 < len(words) and words[idx+1] in index: mat[wid][2*TOTAL+index[words[idx+1]]] += 1
+            if idx+2 < len(words) and words[idx+2] in index: mat[wid][3*TOTAL+index[words[idx+2]]] += 1
+            #if idx+3 < len(words) and words[idx+3] in index: mat[wid][index[words[idx+3]]] += 0.5
     with open(datafile, "wb") as f:
         pickle.dump((article, sentences, dic, index, mwords, mat),f)
 
 
 if __name__ == "__main__":
-    datafile = 'data2.dat'
+    datafile = 'data3.dat'
     article = ''
     sentences = []
     dic = {}
@@ -54,15 +54,13 @@ if __name__ == "__main__":
         (article, sentences, dic, index, mwords, mat) = pickle.load(f)
 
     def pmi():
-        for i in range(TOTAL):
-            #alpha = 1/np.log(mwords[i][1])
-            alpha = 1
-            if len(mwords[i][0])<=3 : alpha = 0
+        for i in range(4*TOTAL):
+            alpha = 1.0/np.log(mwords[i%TOTAL][1])**3
+            #alpha = 1
+            #if len(mwords[i][0])<=3 : alpha = 0
             for j in range(TOTAL):
                 mat[j][i] *= alpha
     pmi()
-    print("words:")
-    print(mwords)
 #test1
     def test1():
         T = input()
@@ -75,9 +73,9 @@ if __name__ == "__main__":
                 print("words doesn't exists")
 
     def test2():
-        T = input()
-        for i in range(eval(T)):
-            pdb.set_trace()
+        #T = input()
+        while True:
+            #pdb.set_trace()
             w1 = input()
             if w1 in index:
                 ls = []
@@ -89,4 +87,26 @@ if __name__ == "__main__":
             else:
                 print("words doesn't exists")
 
-    test2()
+    #test2()
+
+    pdb.set_trace()
+    def test3():
+        vecx = mat[index['happy']]
+        vecy = mat[index['friend']]
+        ax = np.average(vecx)
+        ay = np.average(vecy)
+        vecm = vecx * vecy
+        mm = np.average(vecm)
+        a = set()
+        print("xword:")
+        for i in sorted(enumerate(vecx), key=lambda x:x[1], reverse=1)[0:10]:
+            print(i[0],i[1],int(i[0]/1000),mwords[i[0]%1000][0])
+        print("====================\nyword:")
+        for i in sorted(enumerate(vecy), key=lambda x:x[1], reverse=1)[0:10]:
+            print(i[0],i[1],int(i[0]/1000),mwords[i[0]%1000][0])
+        for i in sorted(enumerate(vecx), key=lambda x:x[1], reverse=1)[0:30]:
+            a.add(i[0])
+        print("====================\nword in commmon:")
+        for i in sorted(enumerate(vecy), key=lambda x:x[1], reverse=1)[0:30]:
+            if i[0] in a: print(i[0],int(i[0]/1000),mwords[i[0]%1000][0])
+    test3()
